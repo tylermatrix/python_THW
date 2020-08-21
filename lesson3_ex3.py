4a14cd7686fdd927d5519db6d72bcf75d8b137d1
tylermatrix <tylermaatrix@gmail.com>
2020-08-20 21:49:58 -0400

    lesson 3

--- lesson3_ex3.py
+++ lesson3_ex3.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 print("How many hours are there in a year?", 365 * 24)
-=======
-print("How many hours are there in a year?", 365 * 24)
->>>>>>> 9eb87757eed72bc6ceffad61be17db26ab65020a
